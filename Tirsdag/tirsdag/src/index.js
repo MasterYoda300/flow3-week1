e635d8dc138a857328871ed92a3ac55c86a6ce79
artin <artin-4000@hotmail.com>
2019-10-29 21:22:16 +0100

    Mandag almost done plus tirsdag halfway

---
+++ Tirsdag/tirsdag/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ListDemoApp2 from './ListDemoApp2';
+
+
+ReactDOM.render(<ListDemoApp2 />, document.getElementById('root'));
+
+
